c889f90022f5a4b6018f136aa80e97711ae3e27c
doris <martynciahotuwa@gmail.com>
2025-03-13 07:33:22 +0100

    chybawork

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  const handleNameChange = (event) => setNewName(event.target.value);
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    const personObject = { name: newName, number: newNumber };
+    setPersons(persons.concat(personObject));
+    setNewName('');
+    setNewNumber('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name} {person.number}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
